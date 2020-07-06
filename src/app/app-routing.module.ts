c672ab97974808a8e41159d78f07b61bff7ab654
elmerv <vasquezelmer101@gmail.com>
2020-07-06 15:21:01 -0700

    login page needs to route to signup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+const routes: Routes = [
+  {path:'' , component: LoginComponent},
+  {path:'./signup', component: SignupComponent}
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
